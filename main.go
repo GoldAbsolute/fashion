a2c074cf619717c2cc63e3e633ea9b6edd997d79
GoldAbsolute <sokoldima1998@mail.ru>
2022-08-13 07:57:07 +0300

    Обработка ошибок в main

--- main.go
+++ main.go
@@ -79,26 +79,31 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		crutime := time.Now().Unix()
 		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		_, err01 := io.WriteString(h, strconv.FormatInt(crutime, 10))
+		check(err01)
 		token := fmt.Sprintf("%x", h.Sum(nil))
 
 		t, _ := template.ParseFiles("upload.gtpl")
-		t.Execute(w, token)
+		err02 := t.Execute(w, token)
+		check(err02)
 	} else {
-		r.ParseMultipartForm(32 << 20)
+		err03 := r.ParseMultipartForm(32 << 20)
+		check(err03)
 		file, handler, err := r.FormFile("uploadfile")
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
+		_, err05 := fmt.Fprintf(w, "%v", handler.Header)
+		check(err05)
 		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 		defer f.Close()
-		io.Copy(f, file)
+		_, err07 := io.Copy(f, file)
+		check(err07)
 	}
 }
